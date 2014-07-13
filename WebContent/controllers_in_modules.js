6f713c5dc5291c8236280c9c8754bad9539a8ca6
pranaykumar <Rimjhim@192.168.2.8>
2014-07-13 23:44:54 +0530

    1. Added new CSS file. 2. Added sample code to illustarte ng-repeat, ng-show, ng-class and ng-style directives.

--- WebContent/controllers_in_modules.js
+++ WebContent/controllers_in_modules.js
@@ -11,20 +11,111 @@ appModule.controller('TextController', function($scope) {
 
 // register StartUpController
 appModule.controller('StartUpController', function($scope) {
-	$scope.funding = {startingEstimate : 0};
-	
+	$scope.funding = {
+		startingEstimate : 0
+	};
 
 	computeNeeded = function() {
 		$scope.funding.needed = $scope.funding.startingEstimate * 10;
 	}
-	
-	$scope.$watch('funding.startingEstimate',computeNeeded);
-	
+
+	$scope.$watch('funding.startingEstimate', computeNeeded);
+
 	$scope.requestFunding = function() {
 		window.alert('Sorry, Get more customers');
 	}
-	
+
 	$scope.reset = function() {
 		$scope.funding.startingEstimate = 0;
+	}
+});
+
+appModule.controller('StudentListController', function($scope) {
+	var students = [ {
+		name : 'Pranay',
+		id : '1'
+	}, {
+		name : 'Prashant',
+		id : '2'
+	}, {
+		name : 'Saket',
+		id : '3'
+	} ];
+
+	var newStudent = '-enter name here-';
+
+	$scope.students = students;
+	$scope.newStudent = newStudent;
+
+	// this function appends a new student in students array at the END
+	$scope.addStudent = function() {
+		$scope.students.splice($scope.students.length, 0, {
+			name : $scope.newStudent,
+			id : $scope.students.length
+		});
+	}
+});
+
+appModule.controller('ToggleMenuController', function($scope) {
+	menuState = {};
+	menuState.show = true;
+	menuState.btnTxt = 'Hide';
+	$scope.menuState = menuState;
+
+	$scope.toggleMenu = function() {
+		$scope.menuState.show = !$scope.menuState.show;
+		if ($scope.menuState.btnTxt == 'Hide')
+			$scope.menuState.btnTxt = 'Show';
+		else
+			$scope.menuState.btnTxt = 'Hide';
+
+	}
+});
+
+appModule
+		.controller(
+				'ErrorWarningController',
+				function($scope) {
+					$scope.isError = false;
+					$scope.isWarning = false;
+					// adding commonStyle object with css styles as key:value
+					// pairs. This can be fed to ng-style directive in the HTML
+					// template
+					$scope.commonStyle = {
+						'background-color' : 'gray',
+						'font-family' : 'monospace',
+						'font-size' : '1.1em'
+					};
+
+					$scope.showError = function() {
+						$scope.isError = true;
+						$scope.isWarning = false;
+						$scope.messageText = 'This is and Error Message!!!';
+					};
+
+					$scope.showWarning = function() {
+						$scope.isError = false;
+						$scope.isWarning = true;
+						$scope.messageText = 'This is just a Warning Message. You can carry on!';
+					};
+
+				});
+appModule.controller('RestaurantController', function($scope) {
+	$scope.directory = [ {
+		name : 'McDonalds',
+		cuisine : 'Fast Food',
+		rating : '4'
+	}, {
+		name : 'Red Lobster',
+		cuisine : 'Sea Food',
+		rating : '5'
+	}, {
+		name : 'Panda Express',
+		cuisine : 'Continental',
+		rating : '4'
+	} ];
+		
+	$scope.selectRestaurant = function(row) {
+	$scope.selectedRow = row;
 	}
 });
